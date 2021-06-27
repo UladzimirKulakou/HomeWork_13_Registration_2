069cf134f884a2961692ec9c3488b6798b7eebd1
UladzimirKulakou <rombik9@ya.ru>
2021-06-27 23:14:21 +0300

    add sing up

---
+++ HomeWork_13_Registration_2/HomeWork_13_Registration_2/Controllers/SingIn/SingInVC.swift
@@ -0,0 +1,39 @@
+//
+//  SingInVC.swift
+//  HomeWork_13_Registration_2
+//
+//  Created by Владимир  on 6/27/21.
+//  Copyright © 2021 Владимир . All rights reserved.
+//
+
+import UIKit
+
+class SingInVC: UIViewController {
+
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passTF: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func emailTFChanged(_ sender: UITextField) {
+    }
+    
+    @IBAction func passTFChanged(_ sender: UITextField) {
+    }
+    
+}
